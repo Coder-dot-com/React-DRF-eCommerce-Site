898a95e14e357ae54b3e90523c6477e8ffe5878b
usman17.s@hotmail.com <usman17.s@hotmail.com>
2022-11-24 21:33:47 +0000

    Add cart action and reducer

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,40 +1,52 @@
-import React, {useState, useEffect} from 'react'
-import { Link } from 'react-router-dom'
-import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap'
-import Rating from '../components/Rating'
-import Loader from '../components/Loader'
-import Message from '../components/Message'
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import {
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  Button,
+  Card,
+  Form,
+} from "react-bootstrap";
+import Rating from "../components/Rating";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 
-import { useParams } from 'react-router'
-import { useDispatch, useSelector } from 'react-redux'
-import { listProductDetails } from '../actions/productActions'
-import axios from 'axios'
+import { useParams } from "react-router";
+import { useDispatch, useSelector } from "react-redux";
+import { listProductDetails } from "../actions/productActions";
+import axios from "axios";
 
 function ProductScreen() {
+  const [qty, setQty] = useState(1);
   let { id } = useParams();
-  const dispatch = useDispatch()
-  const productDetails  = useSelector(state => state.productDetails)
-  const {loading, product, error} = productDetails
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const productDetails = useSelector((state) => state.productDetails);
+  const { loading, product, error } = productDetails;
 
-  useEffect(()=>{
-    dispatch(listProductDetails(id))
-
-
-  }, [])
-
+  useEffect(() => {
+    dispatch(listProductDetails(id));
+  }, []);
   
+  const addToCartHandler = () => {
+    navigate(`/cart/${id}?qty=${qty}`)
+  };
+
   return (
     <div>
-    <Link to='/' className='btn btn-light my-3'>Go Back</Link>
-    {loading ? 
-    <Loader/>
-      : error ?
-        <Message variant='danger'>{error}</Message>  
-        :(
-          <Row>
+      <Link to="/" className="btn btn-light my-3">
+        Go Back
+      </Link>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <Row>
           <Col md={6}>
-            <Image src={product.image} alt={product.name} fluid/>
-          
+            <Image src={product.image} alt={product.name} fluid />
           </Col>
           <Col md={3}>
             <ListGroup variant="flush">
@@ -42,14 +54,14 @@ function ProductScreen() {
                 <h3>{product.name}</h3>
               </ListGroup.Item>
               <ListGroup.Item>
-                <Rating value={product.rating} text={`${product.numReviews} reviews`} color={'#f8e825'} />
+                <Rating
+                  value={product.rating}
+                  text={`${product.numReviews} reviews`}
+                  color={"#f8e825"}
+                />
               </ListGroup.Item>
-              <ListGroup.Item>
-                ${product.price}
-              </ListGroup.Item>
-              <ListGroup.Item>
-                {product.description}
-              </ListGroup.Item>
+              <ListGroup.Item>${product.price}</ListGroup.Item>
+              <ListGroup.Item>{product.description}</ListGroup.Item>
             </ListGroup>
           </Col>
           <Col md={3}>
@@ -57,40 +69,61 @@ function ProductScreen() {
               <ListGroup variant="flush">
                 <ListGroup.Item>
                   <Row>
+                    <Col>Price:</Col>
                     <Col>
-                      Price:
-                    </Col>
-                    <Col>
-                    <strong>${product.price}</strong>
+                      <strong>${product.price}</strong>
                     </Col>
                   </Row>
                 </ListGroup.Item>
                 <ListGroup.Item>
                   <Row>
+                    <Col>Status:</Col>
                     <Col>
-                      Status:
-                    </Col>
-                    <Col>
-                    <strong>{product.countInStock > 0 ? 'In stock' : 'Out of stock'}</strong>
+                      <strong>
+                        {product.countInStock > 0 ? "In stock" : "Out of stock"}
+                      </strong>
                     </Col>
                   </Row>
                 </ListGroup.Item>
-    
+
+                {product.countInStock > 0 && (
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Qty</Col>
+                      <Col xs="auto" className="my-1">
+                        <Form.Control
+                          as="select"
+                          value={qty}
+                          onChange={(e) => setQty(e.target.value)}
+                        >
+                          {[...Array(product.countInStock).keys()].map((x) => (
+                            <option key={x + 1} value={x + 1}>
+                              {x + 1}
+                            </option>
+                          ))}
+                        </Form.Control>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                )}
+
                 <ListGroup.Item>
-                  <Button className='btn-block w-100' disabled={product.countInStock > 0 ? false : true} type='button'>Add to cart</Button>
+                  <Button
+                    className="btn-block w-100"
+                    onClick={addToCartHandler}
+                    disabled={product.countInStock > 0 ? false : true}
+                    type="button"
+                  >
+                    Add to cart
+                  </Button>
                 </ListGroup.Item>
               </ListGroup>
             </Card>
-    
           </Col>
-    
         </Row>
-        )
-  }
-
-
+      )}
     </div>
-  )
+  );
 }
 
-export default ProductScreen
+export default ProductScreen;
