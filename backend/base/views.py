fe586907cb5a8b374ed2161633e05894f6e2a2e7
usman17.s@hotmail.com <usman17.s@hotmail.com>
2022-11-24 10:29:27 +0000

    Add action and reducer for productlist

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,19 +1,42 @@
-from django.shortcuts import render
-from django.http import JsonResponse
 
-from .models import Product
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializers import ProductSerializer
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+from base.models import Product
+from base.serializers import ProductSerializer
+
 
 # Create your views here.
 
 
 @api_view(['GET'])
 def getProducts(request):
-    products = Product.objects.all()
+    query = request.query_params.get('keyword')
+    if query == None:
+        query = ''
+
+    products = Product.objects.filter(
+        name__icontains=query).order_by('-createdAt')
+
+    page = request.query_params.get('page')
+    paginator = Paginator(products, 5)
+
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+
+    if page == None:
+        page = 1
+
+    page = int(page)
+    print('Page:', page)
     serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
+    return Response({'products': serializer.data, 'page': page, 'pages': paginator.num_pages})
 
 @api_view(['GET'])
 def getProduct(request, pk):
