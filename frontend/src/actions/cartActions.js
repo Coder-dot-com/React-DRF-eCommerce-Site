6efee624718bfb758abc4347c005e23ee70c2f52
usman17.s@hotmail.com <usman17.s@hotmail.com>
2022-11-25 10:19:15 +0000

    Cart actions reducers and screen

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import {
     CART_ADD_ITEM,
+    CART_REMOVE_ITEM
 
 } from '../constants/cartConstants'
 
@@ -19,4 +20,16 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
         }
     })
     localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
+}
+
+export const removeFromCart = (id) => (dispatch, getState) => {
+    dispatch({
+        type: CART_REMOVE_ITEM,
+        payload: id,
+    })
+
+    localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
+
+
+
 }
